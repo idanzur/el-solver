b153f9f1963846db11f028de6424df92270e266e
idanzur <idanzur97@gmail.com>
2022-07-20 13:01:24 +0300

    emovi: ts file name changed

--- src/solvers/emovi.js
+++ src/solvers/emovi.js
@@ -3,7 +3,7 @@
     const appFile = await readFileFromSourceMap(url, 'App.tsx');
     const today = new Date().toISOString().split('T')[0];
     const movieId = new RegExp(`"${today}":.*?id:\\s*"(.*?)"`).exec(appFile)[1];
-    const moviesFile = await readFileFromSourceMap(url, 'top250movies.ts');
+    const moviesFile = await readFileFromSourceMap(url, 'movies.ts');
     const answer = new RegExp(`${movieId}(?:.|\\n)*?title:\\s*"(.*?)"`).exec(moviesFile)[1]
     inputCountryAnswer(answer);
     document.querySelectorAll('*[id^="react-select-2-option"]')[0].click();
