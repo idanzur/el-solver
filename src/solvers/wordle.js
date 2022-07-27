b2e95804aa767dae0ed7f69c48a91e2e7b8c052c
idanzur <idanzur97@gmail.com>
2022-07-27 23:34:55 +0300

    lint

--- src/solvers/wordle.js
+++ src/solvers/wordle.js
@@ -1,9 +1,8 @@
-
 (async () => {
     const jsUrl = document.querySelector('link[rel=stylesheet]').href.replace(/\.css$/, '.js');
     const data = await (await fetch(jsUrl)).text();
     const words = JSON.parse(/\["cigar".*?]/.exec(data)[0]);
     const index = Math.round((new Date().setHours(0, 0, 0, 0) - 1624050000000) / 86400000)
     const answer = words[index];
-    [...answer,  '↵'].forEach( c => document.querySelector(`[data-key='${c}']`).click());
+    [...answer, '↵'].forEach(c => document.querySelector(`[data-key='${c}']`).click());
 })();
