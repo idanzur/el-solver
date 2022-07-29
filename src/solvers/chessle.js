c0092024aca7d1d70cdbdd3b12b039a4e33077ae
idanzur <idanzur97@gmail.com>
2022-07-29 19:44:42 +0300

    solve chessle in normal mode

--- src/solvers/chessle.js
+++ src/solvers/chessle.js
@@ -1,6 +1,7 @@
 (async () => {
     const data = await (await fetch(SERVER_URL_GET)).json();
-    data.moves.forEach(m => {
+    const moveCount = difficulty == 'n' ? 6 : 12;
+    data.moves.slice(0, moveCount).forEach(m => {
         game.move(m);
         syncGameBoard();
     });
