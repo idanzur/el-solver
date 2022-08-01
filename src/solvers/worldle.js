cdf0a4650d5b45db44d7fa2fa5580b0d92c3bc50
idanzur <idanzur97@gmail.com>
2022-08-01 15:43:15 +0300

    bug fix in worldle

--- src/solvers/worldle.js
+++ src/solvers/worldle.js
@@ -1,13 +1,8 @@
+
 (async () => {
     const data = await readFileFromSourceMap(document.querySelector("script[defer]").src, "countries.position.ts");
-    const jsObject = data
-        .split("=")[1]
-        .replace(',\n];', ']')
-        .replace(/(\w+):/g, '"$1":')
-        .replace(/,\n  }/g, '}');
-    const countries = JSON.parse(jsObject);
     const countryCode = /\/([a-z]{2})\//.exec(document.querySelector('img[alt="country to guess"]').src)[1].toUpperCase();
-    const country = countries.find(c => c.code === countryCode)
-    inputCountryAnswer(country.name);
+    const country = new RegExp(`code:\\s*"${countryCode}"[\\s\\S]+?name:\\s*"(.*?)"`, 'g').exec(data)[1]
+    inputCountryAnswer(country);
     document.querySelector('button[type=submit]').click();
 })();
