0f6438e74396b3fd70331d0a8fe5880247bee88d
idanzur <idanzur97@gmail.com>
2022-08-07 15:15:09 +0300

    fix querySelector

--- src/solvers/german-wordle.js
+++ src/solvers/german-wordle.js
@@ -1,5 +1,5 @@
 (() => {
-    const buttons = Array.from(document.querySelectorAll('div[class=key]'));
+    const buttons = Array.from(document.querySelectorAll('div.key'));
     const letters = 'QWERTZUIOPASDFGHJKL~YXCVBNM@';
     const answer = app.getTodaysGameData().solution;
     [...answer, '~'].forEach(c => buttons[letters.indexOf(c)].click())
